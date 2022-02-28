b9c5465344b25f5081a8103662821ccb5e9c4326
Max Herman <maxbherman12@gmail.com>
2022-02-28 20:55:01 +0100

    initial commit

---
+++ app/src/main/java/com/example/come/ui/post/PostViewModel.java
@@ -0,0 +1,7 @@
+package com.example.come.ui.post;
+
+import androidx.lifecycle.ViewModel;
+
+public class PostViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/come/ui/profile/ProfileViewModel.java
@@ -0,0 +1,7 @@
+package com.example.come.ui.profile;
+
+import androidx.lifecycle.ViewModel;
+
+public class ProfileViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
